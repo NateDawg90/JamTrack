49767672311378113874b6bf78e3ac5dbb35f734
Nathan Johnson <rnatejohnson@gmail.com>
2023-05-02 20:26:37 -0500

    test without openapi

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -6,7 +6,6 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  console.log({ configuration, process: process.env });
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -26,29 +25,29 @@ export default async function (req, res) {
     return;
   }
 
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(req.body),
-      temperature: 0.6,
-      max_tokens: 600,
-    });
+  // try {
+  //   const completion = await openai.createCompletion({
+  //     model: "text-davinci-003",
+  //     prompt: generatePrompt(req.body),
+  //     temperature: 0.6,
+  //     max_tokens: 600,
+  //   });
 
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch (error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: "An error occurred during your request.",
-        },
-      });
-    }
-  }
+  //   res.status(200).json({ result: completion.data.choices[0].text });
+  // } catch (error) {
+  //   // Consider adjusting the error handling logic for your use case
+  //   if (error.response) {
+  //     console.error(error.response.status, error.response.data);
+  //     res.status(error.response.status).json(error.response.data);
+  //   } else {
+  //     console.error(`Error with OpenAI API request: ${error.message}`);
+  //     res.status(500).json({
+  //       error: {
+  //         message: "An error occurred during your request.",
+  //       },
+  //     });
+  //   }
+  // }
 }
 
 function generatePrompt(reqBody) {
