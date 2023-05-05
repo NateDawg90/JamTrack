ab24e41a813c691a94fbcd3754a2898eaa562442
Nathan Johnson <rnatejohnson@gmail.com>
2023-05-05 16:22:56 -0700

    add a bunch of stuff

--- components/TextInput/index.tsx
+++ components/TextInput/index.tsx
@@ -1,17 +1,25 @@
-import { FC } from "react";
+import { Dispatch, FC, SetStateAction } from "react";
 import { TextField } from "./text-field.styles";
 
 interface Props {
-  value: string;
-  setValue: (value: string) => void;
+  value: string | number;
+  setValue: Dispatch<SetStateAction<string | number>>;
   label: string;
   placeholder: string;
+  type?: string;
 }
-const TextInput: FC<Props> = ({ value, setValue, label, placeholder }) => {
+const TextInput: FC<Props> = ({
+  value,
+  setValue,
+  label,
+  placeholder,
+  type = "text",
+}) => {
   return (
-    <div className="d-flex align-items-center justify-content-between mb-4">
+    <div className="d-flex align-items-center justify-content-between mb-3">
       {label}:
       <TextField
+        type={type}
         className="ms-2"
         name="Event name"
         placeholder={placeholder}
